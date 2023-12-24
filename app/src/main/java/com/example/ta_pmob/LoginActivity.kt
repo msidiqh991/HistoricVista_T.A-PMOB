c6fdcb050244ae638851d580938138d2c5d918b8
Muhammad Sidiq Hardiansyah <msidiqh991@gmail.com>
2023-12-24 23:01:37 +0700

    LoginActivity connect to RegisterAcitivty

--- app/src/main/java/com/example/ta_pmob/LoginActivity.kt
+++ app/src/main/java/com/example/ta_pmob/LoginActivity.kt
@@ -1,11 +1,26 @@
 package com.example.ta_pmob
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ta_pmob.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+
+        binding.tvHaventAccount.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
     }
 }
