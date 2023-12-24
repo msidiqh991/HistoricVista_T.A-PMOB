bc0f727283877f155fa78fadf30df27432e6dd46
Muhammad Sidiq Hardiansyah <msidiqh991@gmail.com>
2023-12-24 23:09:25 +0700

    Update HomeActivity

--- app/src/main/java/com/example/ta_pmob/HomeActivity.kt
+++ app/src/main/java/com/example/ta_pmob/HomeActivity.kt
@@ -2,10 +2,18 @@ package com.example.ta_pmob
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ta_pmob.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // TODO : MAIN FEATURE APP
+
     }
 }
--- app/src/main/java/com/example/ta_pmob/RegisterActivity.kt
+++ app/src/main/java/com/example/ta_pmob/RegisterActivity.kt
@@ -1,11 +1,26 @@
 package com.example.ta_pmob
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ta_pmob.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnRegister.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        binding.tvHaveAccount.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
     }
 }
