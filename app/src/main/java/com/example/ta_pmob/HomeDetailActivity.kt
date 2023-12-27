d98cd039375c5432fa296bea5be738788288e930
Muhammad Sidiq Hardiansyah <msidiqh991@gmail.com>
2023-12-28 02:03:47 +0700

    fix redirect to Login + HomeDetailActivity

--- app/src/main/java/com/example/ta_pmob/HomeDetailActivity.kt
+++ app/src/main/java/com/example/ta_pmob/HomeDetailActivity.kt
@@ -1,11 +1,13 @@
 package com.example.ta_pmob
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class HomeDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_detail)
+
+//        TODO: ?
     }
 }
