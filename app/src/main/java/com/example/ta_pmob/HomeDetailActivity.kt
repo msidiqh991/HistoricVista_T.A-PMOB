be070c61c1b5ac6f981b110e8100ef4ea19588fc
Muhammad Sidiq Hardiansyah <msidiqh991@gmail.com>
2024-01-12 00:00:37 +0700

    checkpoint new

--- app/src/main/java/com/example/ta_pmob/HomeDetailActivity.kt
+++ app/src/main/java/com/example/ta_pmob/HomeDetailActivity.kt
@@ -60,7 +60,6 @@ class HomeDetailActivity : AppCompatActivity() {
                             dataWisata.toString(),
                             dataKota.toString(),
                         )
-                        break
                     }
                 }
             }
--- app/src/main/java/com/example/ta_pmob/models/MapsImageModel.kt
+++ app/src/main/java/com/example/ta_pmob/models/MapsImageModel.kt
@@ -7,4 +7,10 @@ data class MapsImageModel(
     val namaWisata: String? = null,
     val namaKota: String? = null,
     val photoUrl: String? = null,
+
+//    val description: String? = null,
+//    val country: String? = null,
+//    val rating: Float? = null,
+//    val jarakSatuan: Int? = null
+
 ) : Serializable
